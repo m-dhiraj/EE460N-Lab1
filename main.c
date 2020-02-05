ef248d8dfcf843e54f6ead851e539e33199b5d72
Kolbe Bauer <43128406+kolbe-bauer@users.noreply.github.com>
2020-02-04 22:17:43 -0600

    Symbol Table beginnings
    We have th ebeginnings of a symbol table

--- main.c
+++ main.c
@@ -5,174 +5,161 @@
 #include <limits.h> /* Library for definitions of common variable type characteristics */
 
 #define MAX_LINE_LENGTH 255
+
 FILE* infile = NULL;
 FILE* outfile = NULL;
-int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
-        ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
-        );
-int main (int argc, char* argv[]){
-    //char *prgName   = argv[0];
-    // char *iFileName = argv[1];
-    // char *oFileName = argv[2];
 
-    //printf("program name = '%s'\n", prgName);
-    //printf("input file name = '%s'\n", iFileName);
-    //printf("output file name = '%s'\n", oFileName);
+enum
+{
+    DONE, OK, EMPTY_LINE
+};
+
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4);
+int isOpcode(char * word);
+
+#define MAX_LABEL_LEN 20
+#define MAX_SYMBOLS 255
+typedef struct {
+	int address;
+	char label[MAX_LABEL_LEN + 1];
+} TableEntry;
+TableEntry symbolTable[MAX_SYMBOLS];
+
+int main (int argc, char* argv[]){
     infile = fopen("kolbe.txt", "r");
-    //infile = fopen("argv[1]", "r");
+    outfile = fopen("output.txt", "w");
+    //infile = fopen(argv[1], "r");
     //outfile = fopen(argv[2], "w");
                 
     if (!infile) {
       printf("Error: Cannot open file %s\n", argv[1]);
       exit(4);
-                 }
-    // if (!outfile) {
-    //   printf("Error: Cannot open file %s\n", argv[2]);
-    //   exit(4);
-    // }
+    }
+    if (!outfile) {
+      printf("Error: Cannot open file %s\n", argv[2]);
+      exit(4);
+    }
 
     /* Do stuff with files */
-    char * pLine;
-    char ** pLabel;
-    char ** pOpcode;
-    char ** pArg1;
-    char ** pArg2;
-    char ** pArg3;
-    char ** pArg4;
-    if(readAndParse(infile, pLine, pLabel, pOpcode, pArg1, pArg2, pArg3, pArg4)==0)
-    printf("reading:\n");
-    
+    char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1, *lArg2, *lArg3, *lArg4;
+    int parseRet;
+    int lCount=0;
+    do{
+        parseRet=readAndParse(infile,lLine,&lLabel,&lOpcode,&lArg1,&lArg2,&lArg3,&lArg4);
+        if(parseRet!=DONE && parseRet!=EMPTY_LINE){
+            //fprintf(outfile, "%d",count);
+            //fprintf(outfile,lLabel);
+            TableEntry t;
+            t.address=lCount;
+            strcpy(t.label,lLabel);
+            symbolTable[lCount]=t;
+            lCount++;
+        }
+    }while (parseRet!=DONE);
+
+    int i;
+    for(i=0;i<lCount;i++){
+        if(strlen(symbolTable[i].label)!=0){
+            printf("%d",symbolTable[i].address);
+            printf(symbolTable[i].label);
+            printf("\n");
+        }
+    }
 
     fclose(infile);
-    //fclose(outfile);
+    fclose(outfile);
 }
 
-int toNum( char * pStr )
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4)
+{
+    char * lRet, * lPtr;
+    int i;
+    if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
+        return( DONE );
+    for( i = 0; i < strlen( pLine ); i++ )
+        pLine[i] = tolower( pLine[i] );
+    /* convert entire line to lowercase */
+    *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+    /* ignore the comments */
+    lPtr = pLine;
+
+    while( *lPtr != ';' && *lPtr != '\0' &&
+    *lPtr != '\n' )
+        lPtr++;
+
+    *lPtr = '\0';
+    if( !(lPtr = strtok( pLine, "\t\n ," ) ) )
+        return( EMPTY_LINE );
+
+    if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
     {
-    char * t_ptr;
-    char * orig_pStr;
-    int t_length,k;
-    int lNum, lNeg = 0;
-    long int lNumLong;
-
-    orig_pStr = pStr;
-    if( *pStr == '#' )                                /* decimal */
-    {
-        pStr++;
-        if( *pStr == '-' )                                /* dec is negative */
-        {
-        lNeg = 1;
-        pStr++;
-        }
-        t_ptr = pStr;
-        t_length = strlen(t_ptr);
-        for(k=0;k < t_length;k++)
-        {
-        if (!isdigit(*t_ptr))
-        {
-            printf("Error: invalid decimal operand, %s\n",orig_pStr);
-            exit(4);
-        }
-        t_ptr++;
-        }
-        lNum = atoi(pStr);
-        if (lNeg)
-        lNum = -lNum;
-
-        return lNum;
+        *pLabel = lPtr;
+        if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
     }
-    else if( *pStr == 'x' )        /* hex     */
-    {
-        pStr++;
-        if( *pStr == '-' )                                /* hex is negative */
-        {
-        lNeg = 1;
-        pStr++;
-        }
-        t_ptr = pStr;
-        t_length = strlen(t_ptr);
-        for(k=0;k < t_length;k++)
-        {
-        if (!isxdigit(*t_ptr))
-        {
-            printf("Error: invalid hex operand, %s\n",orig_pStr);
-            exit(4);
-        }
-        t_ptr++;
-        }
-        lNumLong = strtol(pStr, NULL, 16);    /* convert hex string into integer */
-        lNum = (lNumLong > INT_MAX)? INT_MAX : lNumLong;
-        if( lNeg )
-        lNum = -lNum;
-        return lNum;
-    }
-    else
-    {
-            printf( "Error: invalid operand, %s\n", orig_pStr);
-            exit(4);  /* This has been changed from error code 3 to error code 4, see clarification 12 */
-    }
-    }
+    if(pLine[0]!='.')
+    *pOpcode = lPtr;
 
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    
+    *pArg1 = lPtr;
+    
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
 
-        enum
-        {
-           DONE, OK, EMPTY_LINE
-        };
+    *pArg2 = lPtr;
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
 
-        int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
-        ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
-        )
-        {
-           char * lRet, * lPtr;
-           int i;
-           if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
-                return( DONE );
-           for( i = 0; i < strlen( pLine ); i++ )
-                pLine[i] = tolower( pLine[i] );
-          printf(pLine);
-          /* convert entire line to lowercase */
-           *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+    *pArg3 = lPtr;
 
-           /* ignore the comments */
-           lPtr = pLine;
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
 
-           while( *lPtr != ';' && *lPtr != '\0' &&
-           *lPtr != '\n' )
-                lPtr++;
+    *pArg4 = lPtr;
 
-           *lPtr = '\0';
-           if( !(lPtr = strtok( pLine, "\t\n ," ) ) )
-                return( EMPTY_LINE );
-
-           if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
-           {
-                *pLabel = lPtr;
-                if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-           }
-          
-          *pOpcode = lPtr;
-
-           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-          
-          *pArg1 = lPtr;
-          
-          if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-
-           *pArg2 = lPtr;
-           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-
-           *pArg3 = lPtr;
-
-           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-
-           *pArg4 = lPtr;
-
-           return( OK );
-        }
-        int isOpcode(char * word){
-            return -1;
-
-            if(isalnum(word))
-            return -1;
-            else return 0;
-        }
+    return( OK );
+}
+int isOpcode(char * word){
+    if(strcmp(word,"add")==0)
+        return 1;
+    if(strcmp(word,"and")==0)
+        return 1;
+    if(strcmp(word,"halt")==0)
+        return 1;
+    if(strcmp(word,"jmp")==0)
+        return 1;
+    if(strcmp(word,"jsr")==0)
+        return 1;
+    if(strcmp(word,"jsrr")==0)
+        return 1;
+    if(strcmp(word,"ldb")==0)
+        return 1;
+    if(strcmp(word,"ldb")==0)
+        return 1;
+    if(strcmp(word,"ldw")==0)
+        return 1;
+    if(strcmp(word,"lea")==0)
+        return 1;
+    if(strcmp(word,"nop")==0)
+        return 1;
+    if(strcmp(word,"not")==0)
+        return 1;
+    if(strcmp(word,"ret")==0)
+        return 1;
+    if(strcmp(word,"lshf")==0)
+        return 1;
+    if(strcmp(word,"rshfl")==0)
+        return 1;
+    if(strcmp(word,"rshfa")==0)
+        return 1;
+    if(strcmp(word,"rti")==0)
+        return 1;
+    if(strcmp(word,"stb")==0)
+        return 1;
+    if(strcmp(word,"stw")==0)
+        return 1;
+    if(strcmp(word,"trap")==0)
+        return 1;
+    if(strcmp(word,"xor")==0)
+        return 1;
+    
+    return -1;
+}
