8513c10b2dc59cdf97cb3f113b354ebbea9f45c6
m-dhiraj <mdhiraj@utexas.edu>
2020-02-04 20:25:18 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #include <string.h> /* String operations library */
 #include <ctype.h> /* Library for useful character operations */
 #include <limits.h> /* Library for definitions of common variable type characteristics */
-#include "parse.h"
+
+#define MAX_LINE_LENGTH 255
 FILE* infile = NULL;
 FILE* outfile = NULL;
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
+        ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
+        );
 int main (int argc, char* argv[]){
     //char *prgName   = argv[0];
     // char *iFileName = argv[1];
@@ -109,3 +113,66 @@ int toNum( char * pStr )
     }
     }
 
+
+        enum
+        {
+           DONE, OK, EMPTY_LINE
+        };
+
+        int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
+        ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
+        )
+        {
+           char * lRet, * lPtr;
+           int i;
+           if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
+                return( DONE );
+           for( i = 0; i < strlen( pLine ); i++ )
+                pLine[i] = tolower( pLine[i] );
+          printf(pLine);
+          /* convert entire line to lowercase */
+           *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+           /* ignore the comments */
+           lPtr = pLine;
+
+           while( *lPtr != ';' && *lPtr != '\0' &&
+           *lPtr != '\n' )
+                lPtr++;
+
+           *lPtr = '\0';
+           if( !(lPtr = strtok( pLine, "\t\n ," ) ) )
+                return( EMPTY_LINE );
+
+           if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+           {
+                *pLabel = lPtr;
+                if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+           }
+          
+          *pOpcode = lPtr;
+
+           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+          
+          *pArg1 = lPtr;
+          
+          if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+           *pArg2 = lPtr;
+           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+           *pArg3 = lPtr;
+
+           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+           *pArg4 = lPtr;
+
+           return( OK );
+        }
+        int isOpcode(char * word){
+            return -1;
+
+            if(isalnum(word))
+            return -1;
+            else return 0;
+        }
